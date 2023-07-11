5116ed909f398ac74ec70663186c72c6d824b05f
Kleiton Linneker <134451444+kleitonlinneker@users.noreply.github.com>
2023-07-11 00:15:31 -0300

    Create 3a Avaliação - Projeto BD (Definição do Problema).sql

---
+++ 3a Avaliação - Projeto BD (Definição do Problema).sql
@@ -0,0 +1,14 @@
+/*
+BANCO DE DADOS DE IMOBILIÁRIA QUE TRABALHA COM LOCAÇÕES DE IMÓVEIS
+
+Uma Imobiliária Regional trabalha com locações de imóveis e deseja um banco de dados para gerenciar suas informações.
+O banco de dados deve conter informações sobre funcionários (que podem ser corretores ou adm), inquilinos (que podem ser PF ou PJ),
+imóveis, proprietários (que podem ser PF ou PJ), condomínios, pagamentos de aluguel, manutenções e prestadores de serviços.
+Um corretor pode atender vários inquilinos e um inquilino pode ser atendido por um ou vários corretores. Um inquilino pode alugar vários
+imóveis cujos também podem ser alugados por vários inquilinos. Quando um inquilino aluga um imóvel são gerados os seguintes dados: CRECI do
+Corretor que realizou a locação, data de início, data de fim, valor e garantia, bem como as informações dos pagamentos de aluguel.
+Um imóvel pode pertencer a nenhum ou a um condomínio e pode ter apenas um proprietário, em um condomínio podem ter um ou vários imóveis e um
+proprietário pode possuir vários imóveis. Os imóveis podem precisar passar por nenhuma ou várias manutenções, mas uma manutenção pode ser feita
+em apenas um imóvel, estas manutenções devem ser cuidadas por um funcionário adm, e os funcionários adm podem cuidar de várias manutenções.
+Cada manutenção precisa ser realizada por um prestador de serviço, que por sua vez pode realizar várias manutenções.
+*/
